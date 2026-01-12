e2e2a43453c616ddeb4890e758cc4eb8eeb625a9
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2026-01-13 02:26:12 +0300

    feat: update competition names and links for improved clarity and relevance

--- be-scraper-fastapi/app/services/unify/lists.py
+++ be-scraper-fastapi/app/services/unify/lists.py
@@ -1,276 +1,286 @@
 ar_names = """
-مسابقة المواصلات الذّكيّة
+مسابقة 5G والذكاء الاصطناعي للسلامة المرورية الذكية
 مسابقة ابتكار التكنولوجيا الحيوية
 مسابقة البلوكتشين
-مسابقة تقنيّات البيئة والطّاقة
+مسابقة محاكاة السيارة الطائرة
 مسابقة تصميم الشرائح الإلكترونية
 مسابقة الصاروخ ذو الهبوط العمودي
-مسابقة تقنيات التّعليم
-مسابقة تقنيات المعيشة الخالية من العوائق
+مسابقة التجارة الإلكترونية
+مسابقة مدن المستقبل المستدامة
+مسابقة تحسين سلسلة التوريد واللوجستيات الذكية
 مسابقة التكنولوجيا المالية
-مسابقة الأمن السيبراني
-مسابقة أنظمة الدفاع الجوي
-مسابقة تصميم الهيلوكبتر
+مسابقة الاتصالات الفضائية الآمنة
+مسابقة الذكاء الاصطناعي في الطيران
+مسابقة أنظمة الدفاع الجوي الفولاذية
 مسابقة تطوير الهايبرلوب
-مسابقة التكنولوجيا لصالح البشرية
 مسابقة المركبات البحرية غير المأهولة
-مسابقة أنظمة الغواصات بدون إنسان
+مسابقة المركبات الأرضية غير المأهولة
+مسابقة أنظمة الغواصات غير المأهولة
 مسابقة تصميم محرك نفّاث
-مسابقة الاتصالات اللاسلكية
-مسابقة الحوسبة الكموميّة
-
-
-
-
+مسابقة تقنيات الكم
+مسابقة الطائرات بدون طيار للمدارس الثانوية
+مسابقة مشاريع بحث تغير المناخ لطلاب المدارس الثانوية
+مسابقة مشاريع البحث القطبي لطلاب المدارس الثانوية
 مسابقة نموذج الأقمار الصناعية
-مسابقة تشخيص وعلاج الأورام
-مسابقة تصحيح الأخطاء والاقتراحات في نظام التّشغيل pardus
-مسابقة التطبيقات التكنولوجية في علم النفس
 مسابقة تصميم تقنيات الطاقة النووية
-مسابقة العربة ذاتية القيادة
+مسابقة 3T في علم الأورام
+مسابقة اقتراحات وإيجاد أخطاء Pardus
+مسابقة روبوتاكسي - مركبة ذاتية القيادة بحجم كامل
 مسابقة الصواريخ
 مسابقة الذكاء الاصطناعي في الصحة
-مسابقة التكنولوجيات الرقمية في الصناعة
-مسابقة المسيرات المقاتلة
-مسابقة قطيع الدرونات
-مسابقة التقنيات الزراعية
-مسابقة دوري الدرونات من تكنوفست
-مسابقة دوري الروبوتات من تكنوفست
-مسابقة تقنيّات السّفر
-مسابقة الدكتوراه وجائزة tuba
-مسابقة معالجة اللغة الطبيعية التركية
-مسابقة الذكاء الاصطناعي في نموذج اللغة T3
-مسابقة الذكاء الاصطناعي في الطيران
-مسابقة السيارات الكهربائية الدولية
-مسابقات مشاريع بحثية لطلبة الجامعات
-كأس العالم للطائرات بدون طيار
-الطائرة بدون طيار (المسابقة الدولية)
-مسابقة تكنوفست للتصميم المعماري والبصري
-
-مسابقة المركبات الأرضية غير المأهولة
-هاكاثون التجارة الإلكترونية
-مسابقة تحديد المواقع بتقنية الجيل الخامس
+مسابقة التقنيات الرقمية في الصناعة
+مسابقة الطائرة بدون طيار المقاتلة
 مسابقة الصواريخ تحت الماء
+مسابقة أسراب الطائرات بدون طيار
+مسابقة التقنيات الزراعية
+بطولة TEKNOFEST للطائرات بدون طيار
+مسابقة TEKNOFEST للتصميم المعماري والبصري
+مسابقة TEKNOFEST للروبوتات
+هاكاثون السفر
+جوائز TÜBA-TEKNOFEST للدكتوراه العلمية
+مسابقة التكنولوجيا لصالح البشرية - المستوى الابتدائي
+مسابقة التكنولوجيا لصالح البشرية - المستوى المتوسط
+مسابقة التكنولوجيا لصالح البشرية - مستوى المدرسة الثانوية
+سباقات السيارات الكهربائية للتحدي الدولي للكفاءة
+مسابقة الطائرات بدون طيار الدولية
+مسابقات مشاريع بحوث طلاب الجامعات
+كأس العالم للطائرات بدون طيار
+مسابقة معالجة اللغة الطبيعية التركية
+مسابقة الحرب الإلكترونية
+مسابقة نجوم الطائرات بدون طيار المقاتلة
+مسابقة TEKNOFEST لأفلام الذكاء الاصطناعي
+مسابقة تتبع طائرات FPV بدون طيار
+مسابقة تقنيات التعدين
+مسابقة TEKNOFEST للمهارات المهنية
+مسابقة تصميم وتشغيل الأنظمة المتقدمة المستقلة
 """
 
 tr_names = """
-Akıllı Ulaşım Yarışması
+5G & Yapay Zeka ile Akıllı Yol Güvenliği Yarışması
 Biyoteknoloji İnovasyon Yarışması
 Blokzincir Yarışması
-Çevre ve Enerji Teknolojileri Yarışması
+Uçan Araba Simülasyon Yarışması
 Çip Tasarım Yarışması
 Dikey İnişli Roket Yarışması
-Eğitim Teknolojileri Yarışması
-Engelsiz Yaşam Teknolojileri Yarışması
+E-Ticaret Yarışması
+Geleceğin Sürdürülebilir Şehirleri Yarışması
+Akıllı Tedarik ve Lojistik Optimizasyonu Yarışması
 Finansal Teknolojiler Yarışması
-HACKMASTERS
-Hava Savunma Sistemleri Yarışması
-Helikopter Tasarım Yarışması
+Güvenli Uydu Haberleşmesi Yarışması
+Havacılıkta Yapay Zeka Yarışması
+Çelikkubbe Hava Savunma Sistemleri Yarışması
 Hyperloop Geliştirme Yarışması
-İnsanlık Yararına Teknoloji Yarışması
 İnsansız Deniz Aracı Yarışması
+İnsansız Kara Aracı Yarışması
 İnsansız Su Altı Sistemleri Yarışması
 Jet Motor Tasarım Yarışması
-Kablosuz Haberleşme Yarışması
-Kuantum Hackathon  Yarışması
+Kuantum Teknolojileri Yarışması
+Liseler Arası İnsansız Hava Araçları Yarışması
 Lise Öğrencileri İklim Değişikliği Araştırma Projeleri Yarışması
 Lise Öğrencileri Kutup Araştırma Projeleri Yarışması
-Liseler Arası Efficiency Challenge Elektrikli Araç Yarışları
-Liseler Arası İnsansız Hava Araçları Yarışması
 Model Uydu Yarışması
+Nükleer Enerji Teknolojileri Tasarım Yarışması
 Onkolojide 3T Yarışması
 Pardus Hata Yakalama ve Öneri Yarışması
-Psikolojide Teknolojik Uygulamalar Yarışması
-Nükleer Enerji Teknolojileri Tasarım Yarışması
 Robotaksi-Binek Otonom Araç Yarışması
 Roket Yarışması
 Sağlıkta Yapay Zeka Yarışması
 Sanayide Dijital Teknolojiler Yarışması
 Savaşan İHA Yarışması
+Su Altı Roket Yarışması
 Sürü İHA Yarışması
 Tarım Teknolojileri Yarışması
 TEKNOFEST Drone Şampiyonası
+TEKNOFEST Mimari ve Görsel Tasarım Yarışması
 TEKNOFEST Robolig Yarışması
 TravelX Ideathon
 TÜBA-TEKNOFEST Doktora Bilim Ödülleri
-Türkçe Doğal Dil İşleme Yarışması
-T3 AI Hackathon
-Havacılıkta Yapay Zeka Yarışması
+İnsanlık Yararına Teknolojiler Yarışması - İlkokul Seviyesi
+İnsanlık Yararına Teknolojiler Yarışması - Ortaokul Seviyesi
+İnsanlık Yararına Teknolojiler Yarışması - Lise Seviyesi
 Uluslararası Efficiency Challenge Elektrikli Araç Yarışları
+Uluslararası İnsansız Hava Aracı Yarışması
 Üniversite Öğrencileri Araştırma Proje Yarışmaları
 World Drone Cup
-Uluslararası İnsansız Hava Aracı Yarışması
-TEKNOFEST Mimari ve Görsel Tasarım Yarışması
-Uluslararası Çocuk Bilim Yarışması
-İnsansız Kara Aracı Yarışması
-E-Ticaret Hackathonu
-5G Konumlandırma Yarışması
-Su Altı Roket Yarışması
+Türkçe Doğal Dil İşleme Yarışması
+Elektronik Harp Yarışması
+Savaşan İHA Yıldızlar Yarışması
+TEKNOFEST Yapay Zeka Film Yarışması
+FPV Drone İzleme (Tracking) Yarışması
+Maden Teknolojileri Yarışması
+TEKNOFEST Mesleki Yetenek Yarışması
+İleri Otonom Sistemler Tasarım ve Operasyon Yarışması
 """
 
 en_name = """
-Smart Transportation Competition
+5G & Artificial Intelligence Smart Road Safety Competition
 Biotechnology Innovation Competition
 Blockchain Competition
-Environment and Energy Technologies Competition
+Flying Car Simulation Competition
 Chip Design Competition
 Vertical Landing Rocket Competition
-Educational Technologies Competition
-Barrıer-Free Lıvıng Technologıes
+E-Commerce Competition
+Sustainable Cities of the Future Competition
+Smart Supply Chain and Logistics Optimization Competition
 Financial Technologies Competition
-HACKMASTERS
-Air Defense Systems Competition
-Helicopter Design Competition
+Secure Satellite Communication Competition
+Artificial Intelligence in Aviation Competition
+Steeldome Air Defense Systems Competition
 Hyperloop Development Competition
-Technology For Humanity Competition
 Unmanned Surface Vehicle Competition
+Unmanned Ground Vehicle Competition
 Unmanned Underwater Systems Competition
 Jet Engine Design Competition
-Wireless Communication Competition
-Quantum Hackathon Competition
+Quantum Technologies Competition
+High School Unmanned Aerial Vehicles Competition
 High School Students Climate Change Research Projects Competition
 High School Students Polar Research Projects Competition
-High School Efficiency Challenge Electric Vehicle Competition
-High School Unmanned Aerial Vehicles Competition
 Model Satellite Competition
-3T in Oncology Competition
-Pardus 21 Debugging and Suggestion Competition
-Technological Applications in Psychology Competition
 Nuclear Energy Technologies Design Competition
+3T in Oncology Competition
+Pardus Debugging and Suggestion Competition
 Robotaxi-Full Scale Autonomous Vehicle Competition
 Rocket Competition
 Artificial Intelligence in Health Competition
 Digital Technologies in Industry Competition
 Fighter UAV Competition
+Underwater Rocket Competition
 Swarm UAV Competition
 Agricultural Technologies Competition
 TEKNOFEST Drone Championship
+TEKNOFEST Architectural and Visual Design Competition
 TEKNOFEST Roboleague Competition
 Travel Hackathon
 TÜBA-TEKNOFEST Doctorate Science Awards
-Turkish Natural Language Processing Competition
-T3 AI Hackathon
-Artificial Intelligence in Transportation Competition
+Technologies for Humanity Competition - Elementary School Level
+Technology for Humanity Competition - Middle School Level
+Technologies for Humanity Competition - High School Level
 International Efficiency Challenge Electric Vehicle Races
+International Unmanned Aerial Vehicle Competition
 University Students Research Projects Competitions
 World Drone Cup
-International Unmanned Aerial Vehicle Competition
-TEKNOFEST Architectural and Visual Design Competition
-International Children Science Competition
-Unmanned Ground Vehicle
-E-Commerce Hackathon
-5G Positioning Competition
-Underwater Rocket Competition
+Turkish Natural Language Processing Competition
+Electronic Warfare Competition
+Fighter UAV Stars Competition
+TEKNOFEST Artificial Intelligence Movie Competition
+FPV Drone Tracking Competition
+Mining Technologies Competition
+TEKNOFEST Vocational Skills Competition
+Advanced Autonomous Systems Design and Operations Competition
 """
 
 tr_link = """
-akilli-ulasim-yarismasi
+5g-yapay-zeka-ile-akilli-yol-guvenligi-yarismasi
 biyoteknoloji-inovasyon-yarismasi
 blokzincir-yarismasi
-cevre-ve-enerji-teknolojileri-yarismasi
+ucan-araba-simulasyon-yarismasi
 cip-tasarim-yarismasi
 dikey-inisli-roket-yarismasi
-egitim-teknolojileri-yarismasi
-engelsiz-yasam-teknolojileri-yarismasi
+e-ticaret-yarismasi
+gelecegin-surdurulebilir-sehirleri-yarismasi
+akilli-tedarik-ve-lojistik-optimizasyonu-yarismasi
 finansal-teknolojiler-yarismasi
-hackmasters
-hava-savunma-sistemleri-yarismasi
-helikopter-tasarim-yarismasi
+guvenli-uydu-haberlesmesi-yarismasi
+havacilikta-yapay-zeka-yarismasi
+celikkubbe-hava-savunma-sistemleri-yarismasi
 hyperloop-gelistirme-yarismasi
-insanlik-yararina-teknoloji-yarismasi
 insansiz-deniz-araci-yarismasi
+insansiz-kara-araci-yarismasi
 insansiz-su-alti-sistemleri-yarismasi
 jet-motor-tasarim-yarismasi
-kablosuz-haberlesme-yarismasi
-kuantum-hackathon-yarismasi
+kuantum-teknolojileri-yarismasi
+liseler-arasi-insansiz-hava-araclari-yarismasi
 lise-ogrencileri-iklim-degisikligi-arastirma-projeleri-yarismasi
 lise-ogrencileri-kutup-arastirma-projeleri-yarismasi
-liseler-arasi-efficiency-challenge-elektrikli-arac-yarislari
-liseler-arasi-insansiz-hava-araclari-yarismasi
 model-uydu-yarismasi
+nukleer-enerji-teknolojileri-tasarim-yarismasi
 onkolojide-3t-yarismasi
 pardus-hata-yakalama-ve-oneri-yarismasi
-psikolojide-teknolojik-uygulamalar-yarismasi
-nukleer-enerji-teknolojileri-tasarim-yarismasi
 robotaksi-binek-otonom-arac-yarismasi
 roket-yarismasi
 saglikta-yapay-zeka-yarismasi
 sanayide-dijital-teknolojiler-yarismasi
 savasan-iha-yarismasi
+su-alti-roket-yarismasi
 suru-iha-yarismasi
 tarim-teknolojileri-yarismasi
 teknofest-drone-sampiyonasi
+teknofest-mimari-ve-gorsel-tasarim-yarismasi
 teknofest-robolig-yarismasi
 travelx-ideathon
 tuba-teknofest-doktora-bilim-odulleri
-turkce-dogal-dil-isleme-yarismasi
-t3-ai-hackathon
-havacilikta-yapay-zeka-yarismasi
+insanlik-yararina-teknolojiler-yarismasi-ilkokul-seviyesi
+insanlik-yararina-teknolojiler-yarismasi-ortaokul-seviyesi
+insanlik-yararina-teknolojiler-yarismasi-lise-seviyesi
 uluslararasi-efficiency-challenge-elektrikli-arac-yarislari
+uluslararasi-insansiz-hava-araci-yarismasi
 universite-ogrencileri-arastirma-proje-yarismalari
 world-drone-cup
-uluslararasi-insansiz-hava-araci-yarismasi
-teknofest-mimari-ve-gorsel-tasarim-yarismasi
-uluslararasi-cocuk-bilim-yarismasi
-insansiz-kara-araci-yarismasi
-e-ticaret-hackathonu
-5g-konumlandirma-yarismasi
-su-alti-roket-yarismasi
+turkce-dogal-dil-isleme-yarismasi
+elektronik-harp-yarismasi
+savasan-iha-yildizlar-yarismasi
+teknofest-yapay-zeka-film-yarismasi
+fpv-drone-izleme-tracking-yarismasi
+maden-teknolojileri-yarismasi
+teknofest-mesleki-yetenek-yarismasi
+ileri-otonom-sistemler-tasarim-ve-operasyon-yarismasi
 """
 
 en_link = """
-smart-transportation-competition
+5g-artificial-intelligence-smart-road-safety-competition
 biotechnology-innovation-competition
 blockchain-competition
-environment-and-energy-technologies-competition
+flying-car-simulation-competition
 chip-design-competition
 vertical-landing-rocket-competition
-educational-technologies-competition
-barrier-free-living-technologies-competition
+e-commerce-competition
+sustainable-cities-of-the-future-competition
+smart-supply-chain-and-logistics-optimization-competition
 financial-technologies-competition
-hackmasters
-air-defense-systems-competition
-helicopter-design-competition
+secure-satellite-communication-competition
+artificial-intelligence-in-aviation-competition
+steeldome-air-defense-systems-competition
 hyperloop-development-competition
-technology-for-humanity-competition
 unmanned-surface-vehicle-competition
+unmanned-ground-vehicle-competition
 unmanned-underwater-systems-competition
 jet-engine-design-competition
-wireless-communication-competition
-quantum-hackathon-competition
+quantum-technologies-competition
+inter-high-school-unmanned-aerial-vehicles-competition
 high-school-students-climate-change-research-projects-competition
 high-school-students-polar-research-projects-competition
-high-school-efficiency-challenge-electric-vehicle-competition
-inter-high-school-unmanned-aerial-vehicles-competition
 model-satellite-competition
-3t-in-oncology-competition
-pardus-debugging-and-suggestion-competition
-technological-applications-in-psychology-competition
 nuclear-energy-technologies-design-competition
+3t-in-oncology-competition
+pardus-suggestion-and-bug-finding-competition
 robotaxi-full-scale-autonomous-vehicle-competition
 rocket-competition
 artificial-intelligence-in-health-competition
 digital-technologies-in-industry-competition
 fighter-uav-competition
+underwater-rocket-competition
 swarm-uav-competition
 agricultural-technologies-competition
 teknofest-drone-championship
+teknofest-architectural-and-visual-design-competition
 teknofest-roboleague-competition
 travelx-ideathon
 tuba-teknofest-doctorate-science-awards
-turkish-natural-language-processing-competition
-t3-ai-hackathon
-aviation-artificial-intelligence-competition
+technologies-for-humanity-competition-elementary-school-level
+technology-for-humanity-competition-middle-school-level
+technologies-for-humanity-competition-high-school-level
 international-efficiency-challenge-electric-vehicle-races
+international-unmanned-aerial-vehicle-competition
 university-students-research-projects-competitions
 world-drone-cup
-international-unmanned-aerial-vehicle-competition
-teknofest-architectural-and-visual-design-competition
-international-children-science-competition
-unmanned-ground-vehicle
-e-commerce-hackathon
-5g-positioning-competition
-underwater-rocket-competitio
+turkish-natural-language-processing-competition
+electronic-warfare-competition
+fighter-uav-stars-competition
+teknofest-artificial-intelligence-movie-competition
+fpv-drone-tracking-competition
+mining-technologies-competition
+teknofest-vocational-skills-competition
+advanced-autonomous-systems-design-and-operations-competition
 """
 
 # Split the multiline strings into lists
