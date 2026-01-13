1c756249abbe23e1f421226a86d5a6a457159fa9
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2026-01-14 00:27:52 +0300

    feat: add AppBindings type and update Hono app initialization

--- be-main-hono/src/index.ts
+++ be-main-hono/src/index.ts
@@ -1,6 +1,7 @@
 import { Hono } from 'hono'
+import type { AppBindings } from './types/env'
 
-const app = new Hono()
+const app = new Hono<AppBindings>();
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
---
+++ be-main-hono/src/types/env.ts
@@ -0,0 +1,7 @@
+export interface Environment {
+  D1_DB: D1Database;
+}
+
+export type AppBindings = {
+  Bindings: Environment;
+};
